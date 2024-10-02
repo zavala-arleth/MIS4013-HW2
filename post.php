9f2178db5e54bcbdf696a94a2d765b33b4cd7601
zavala-arleth <157073222+zavala-arleth@users.noreply.github.com>
2024-10-02 01:35:05 -0500

    Update post.php

--- post.php
+++ post.php
@@ -3,7 +3,7 @@ $pageTitle = "Post";
   include "header.php";
 ?>
     <h1>Post</h1>
-<form method="post"action="post-result";>
+<form method="post" action="post-result";>
   <input type="text" name="my-name">
   <input type="submit" value="Post data">
 <?php
