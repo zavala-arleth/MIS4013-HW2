df8a925ec8e88cf4c398391779afd660347492a3
zavala-arleth <157073222+zavala-arleth@users.noreply.github.com>
2024-10-01 22:58:28 -0500

    Update header.php

--- header.php
+++ header.php
@@ -1,1 +1,36 @@
-
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap demo</title>
+  </head>
+  <body>
+  <div class = "container">
+ <nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Navbar scroll</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarScroll" aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarScroll">
+      <ul class="navbar-nav me-auto my-2 my-lg-0 navbar-nav-scroll" style="--bs-scroll-height: 100px;">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="/">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="post.php">Post</a>
+        </li>
+      <li class="nav-item">
+          <a class="nav-link" href="post-result.php">Post Result</a>
+        </li>
+      <li class="nav-item">
+          <a class="nav-link" href="Get.php">Get</a>
+        </li>
+      <li class="nav-item">
+          <a class="nav-link" href="Get-Result.php">Get Result</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
