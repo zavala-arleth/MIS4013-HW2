25eaac26a56fef8e09fa8ed6a4490e306ccbec41
zavala-arleth <157073222+zavala-arleth@users.noreply.github.com>
2024-10-02 01:41:30 -0500

    Create get.php

---
+++ get.php
@@ -0,0 +1,11 @@
+<?php
+$pageTitle = "Get";
+  include "header.php";
+?>
+    <h1>Get</h1>
+<form method="get"action="get-result.php";>
+  <input type="text" name="my-name">
+  <input type="submit" value="Get data">
+<?php
+include "footer.php";
+?>
